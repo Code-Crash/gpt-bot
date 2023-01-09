f3da48afb1e7b394199c48de5b0e697c78a37207
code-crash <pravintiwari1992@gmail.com>
2023-01-09 19:09:52 +0530

    changed some code

--- index.js
+++ index.js
@@ -37,7 +37,8 @@ app.post('/chat', async (req, res) => {
 app.post('/chat', async (req, res) => {
     try {
         let prompt = '';
-        console.log('req.body.command:', req.body.command, typeof req.body.command, req.body.command === '/askmeanzipy');
+        let command = req.body && req.body.command ? req.body.command : '';
+        console.log('command:', command, typeof command, command === '/askmeanzipy');
         if (req.body && req.body && req.body.command && req.body.command === '/askmeanzipy') {
             console.log(`Sarcastic Mode Selected!`);
             prompt = `Need Sarcastic Answers, ${req.body.text}`;
